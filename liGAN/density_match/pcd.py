ce53668a149c3b2b13fdebf485322a738de30269
Rocco Meli <r.meli@bluemail.ch>
2021-06-22 14:23:51 +0200

    density matching

---
+++ liGAN/density_match/pcd.py
@@ -0,0 +1,11 @@
+import open3d as o3d
+
+pcd = o3d.io.read_point_cloud("test.pcd")
+
+o3d.visualization.draw_geometries(
+    [pcd],
+    #zoom=0.3412,
+    #front=[0.4257, -0.2125, -0.8795],
+    #lookat=[2.6172, 2.0475, 1.532],
+    #up=[-0.0694, -0.9768, 0.2024],
+)
